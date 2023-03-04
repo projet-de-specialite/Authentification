8f6bf3e49b0ed931c15af74fdb308570498e7c74
Chorouq <chorouq1.laarouja@gmail.com>
2023-03-05 00:51:09 +0100

    Application

---
+++ src/test/java/com/exam/auth/Application.java
@@ -0,0 +1,13 @@
+package com.exam.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Application {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
