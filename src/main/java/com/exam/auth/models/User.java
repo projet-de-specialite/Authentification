58e50134b2bd69cf223542fe427d3e27acb9a175
Chorouq <chorouq1.laarouja@gmail.com>
2023-03-12 14:22:28 +0100

    Modifications

--- src/main/java/com/exam/auth/models/User.java
+++ src/main/java/com/exam/auth/models/User.java
@@ -83,4 +83,8 @@ public class User {
   public void setRoles(Set<Role> roles) {
     this.roles = roles;
   }
+
+  public User orElseThrow(Object object) {
+    return null;
+  }
 }
--- src/main/java/com/exam/auth/repository/UserRepository.java
+++ src/main/java/com/exam/auth/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.exam.auth.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,7 @@ import com.exam.auth.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
+  User findByUsername(String username);
 
   Boolean existsByUsername(String username);
 
